c946319001fc9b8488a3e6af8b6918acbb47892d
rx <879977895@qq.com>
2022-02-27 10:28:56 +0800

    init

---
+++ src/main/java/com/example/viki/VikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.viki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/viki/VikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
