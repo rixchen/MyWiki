4c983a51fa905395cc4ca2ef5d9d0c01fb381983
rx <879977895@qq.com>
2022-02-27 21:39:14 +0800

    add http test direction and add yml file

--- src/main/java/com/example/viki/config/VikiApplication.java
+++ src/main/java/com/example/viki/config/VikiApplication.java
@@ -1,13 +1,14 @@
-package com.example.viki;
+package com.example.viki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.example")
 @SpringBootApplication
 public class VikiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VikiApplication.class, args);
 	}
-
 }
